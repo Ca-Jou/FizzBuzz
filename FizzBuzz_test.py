d7394a5fb31cb7b2dd7f5178940609b34a594268
Ca-Jou <camille.pauron@gmail.com>
2021-02-23 21:08:59 +0100

    test 2 OK

--- FizzBuzz_test.py
+++ FizzBuzz_test.py
@@ -1,8 +1,13 @@
 import unittest
 from FizzBuzz import FizzBuzz
 
+
 class FizzBuzzTest(unittest.TestCase):
 
     def test_should_raise_error_when_zero(self):
         with self.assertRaises(ValueError):
             FizzBuzz.answer(0)
+
+    def test_should_raise_error_when_negative(self):
+        with self.assertRaises(ValueError):
+            FizzBuzz.answer(-2)
