fd5cf8d5ce4da06ebe1fd4b50805a710210229e7
Johanna Jato <johanna.jato@gmail.com>
2021-02-23 22:02:53 +0100

    Test 5 is Ok

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -4,7 +4,9 @@ class FizzBuzz:
     def answer(n):
         if n <= 0:
             raise ValueError
-        if n % 3 == 0:
+        if n % 3 == 0  and n % 5 == 0:
+            return 'FizzBuzz'
+        elif n % 3 == 0:
             return 'Fizz'
         elif n % 5 == 0:
             return 'Buzz'
