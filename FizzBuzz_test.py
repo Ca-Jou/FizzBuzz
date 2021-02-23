3436e5f1daaafd41278c23408a1b7304a3a5fc19
Johanna Jato <johanna.jato@gmail.com>
2021-02-23 21:42:33 +0100

    Test3 fail

--- FizzBuzz_test.py
+++ FizzBuzz_test.py
@@ -11,3 +11,8 @@ class FizzBuzzTest(unittest.TestCase):
     def test_should_raise_error_when_negative(self):
         with self.assertRaises(ValueError):
             FizzBuzz.answer(-2)
+
+    def test_should_return_fizz_when_3_is_multiple(self):
+        actual = FizzBuzz.answer(6)
+        expected = 'Fizz'
+        self.assertEqual(expected, actual, 'Should be Fizz GROSSE POMPE ! ')
