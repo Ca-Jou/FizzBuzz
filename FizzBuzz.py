1d912a269c892c7098a1443934eb1c0e45d83845
Ca-Jou <camille.pauron@gmail.com>
2021-02-25 18:11:39 +0100

    refacto

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -4,11 +4,11 @@ class FizzBuzz:
     def answer(n):
         if n <= 0:
             raise ValueError
-        if n % 3 == 0  and n % 5 == 0:
+        elif n % 3 == 0 and n % 5 == 0:
             return 'FizzBuzz'
         elif n % 3 == 0:
             return 'Fizz'
         elif n % 5 == 0:
             return 'Buzz'
-        return n
-
+        else:
+            return n
--- FizzBuzz_test.py
+++ FizzBuzz_test.py
@@ -4,30 +4,26 @@ from FizzBuzz import FizzBuzz
 
 class FizzBuzzTest(unittest.TestCase):
 
-    def test_should_raise_error_when_zero(self):
-        with self.assertRaises(ValueError):
-            FizzBuzz.answer(0)
-
-    def test_should_raise_error_when_negative(self):
+    def test_should_raise_error_when_negative_or_zero(self):
         with self.assertRaises(ValueError):
             FizzBuzz.answer(-2)
 
     def test_should_return_fizz_when_3_is_multiple(self):
         actual = FizzBuzz.answer(6)
         expected = 'Fizz'
-        self.assertEqual(expected, actual, 'Should be Fizz GROSSE POMPE ! ')
+        self.assertEqual(expected, actual, 'Should be Fizz!')
 
-    def test_should_be_buzz_when_5_is_multiple(self):
+    def test_should_return_buzz_when_5_is_multiple(self):
         actual = FizzBuzz.answer(10)
         expected = 'Buzz'
-        self.assertEqual(expected, actual, 'Should be Buzz, GROSSE POMPE !')
+        self.assertEqual(expected, actual, 'Should be Buzz!')
 
-    def test_should_be_fizzbuzz_when_3_and_5_is_multiple(self):
+    def test_should_return_fizzbuzz_when_3_and_5_is_multiple(self):
         actual = FizzBuzz.answer(15)
         expected = 'FizzBuzz'
-        self.assertEqual(expected, actual, 'Should be FizzBuzz, GROSSE POOOOOOOMPE !!!')
+        self.assertEqual(expected, actual, 'Should be FizzBuzz!')
 
-    def test_should_be_n_in_other_case(self):
+    def test_should_return_n_in_other_cases(self):
         actual = FizzBuzz.answer(8)
         expected = 8
-        self.assertEqual(expected, actual, 'Should be 8, GROOOSSE POOOOOOMPE!!')
+        self.assertEqual(expected, actual, 'Should be 8!')
